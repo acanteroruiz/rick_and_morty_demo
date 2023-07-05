f2caaebf03a94a028b534870b5881cbb2586d374
acanteroruiz <acanteroruiz.dev@gmail.com>
2023-07-05 08:30:43 +0200

    Remove uncomplete tests

--- test_driver/app.dart
+++
@@ -1,8 +0,0 @@
-//import 'package:flutter_driver/driver_extension.dart';
-import 'package:rick_and_morty_app/main.dart' as app;
-
-void main() {
-  //enableFlutterDriverExtension();
-
-  app.main();
-}
